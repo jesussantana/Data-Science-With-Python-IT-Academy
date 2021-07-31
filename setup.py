bd8ff43c6aaec111d1da3ccbffdd9b6212b26843
jesussantana <jesus@esmarketingdigital.com>
2021-07-31 14:41:54 +0200

    Project Organization

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='In the Data Science itinerary with Python we will learn how to extract value from data by ingesting, transforming, storing, analyzing and visualizing data.',
+    author='Jesus Santana',
+    license='MIT',
+)
